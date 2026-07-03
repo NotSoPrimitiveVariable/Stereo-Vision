82a6f4d61cb81a8779241f41d7a62e291aa06e5c
Adrian Ross <Adrian.michael.ross08@gmail.com>
2026-07-02 20:11:14 -0400

    Got the real cameras running on my computer there's a bug with the motherboard, so I had to manually set the fps and resolution

--- calibrating_camera/access_camera.py
+++ calibrating_camera/access_camera.py
@@ -1,16 +1,37 @@
+from datetime import datetime
+
 import cv2 as cv
 
-webcam = cv.VideoCapture(0)
+webcam = cv.VideoCapture(2, cv.CAP_V4L2)  # use the /dev/videoX index, not an arbitrary OpenCV index
+
+fourcc = cv.VideoWriter.fourcc(*'MJPG')
+webcam.set(cv.CAP_PROP_FOURCC, fourcc)  # must come before width/height/fps
+webcam.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+webcam.set(cv.CAP_PROP_FRAME_HEIGHT, 800)
+webcam.set(cv.CAP_PROP_FPS, 100)
+
+start = datetime.now()
+frame_number = 0
+
+print("Actual FOURCC:", int(webcam.get(cv.CAP_PROP_FOURCC)))
+print("Actual size:", webcam.get(cv.CAP_PROP_FRAME_WIDTH), webcam.get(cv.CAP_PROP_FRAME_HEIGHT))
+print("Actual FPS reported:", webcam.get(cv.CAP_PROP_FPS))
 
 while True:
     ret, frame = webcam.read()
-
     if not ret:
         break
+    else:
+        frame_number += 1
 
     cv.imshow("Webcam", frame)
+    now = datetime.now()
+    time_difference = now - start
 
+    fps = frame_number / (time_difference.seconds + .00001)
+    print("fps: {}", fps)
     if cv.waitKey(1) & 0xFF == ord("q"):
         break
+
 webcam.release()
 cv.destroyAllWindows()
